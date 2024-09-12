39746959e7d373ec4902ba46fe09b4690a069ef7
Titouan <verdier.titouan@gmail.com>
2024-09-13 00:37:41 +0200

    Main

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.querySelector('.btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#decouvrir').scrollIntoView({ behavior: 'smooth' });
+// Glissement en douceur vers l'ancre
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
+
+// Fonction pour retourner les tuiles
+function flipTile(tile) {
+    tile.querySelector('.front').classList.toggle('flip');
+    tile.querySelector('.back').classList.toggle('flip');
+}
